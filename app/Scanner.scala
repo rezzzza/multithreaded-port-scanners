266cbb110f6c51defc6b8cb9f038bb65fb5bd8f7
Reza Hamouleh <rezahamouleh92@gmail.com>
2020-05-20 20:02:26 +0430

    Add the scanner object

---
+++ app/Scanner.scala
@@ -0,0 +1,61 @@
+import java.net.{InetSocketAddress, Socket}
+import java.util.concurrent.TimeUnit
+
+import scala.concurrent.{ExecutionContext, Future}
+import ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
+import scala.compat.Platform.EOL
+
+object Scanner extends App {
+
+  val FutureTimeOut = Duration(4, TimeUnit.SECONDS)
+  val TCPTimeOut = Duration(3, TimeUnit.SECONDS)
+  val Ports = Range(1, 65535)
+
+  def main(): Unit = {
+    scan(getHost)
+    synchronized {
+      wait()
+    }
+  }
+
+  private def scan(host: String): Unit = {
+    def checkPort(portNumber: Int): Future[Boolean] = {
+      Future {
+        try {
+          val client = JavaSocket().create
+          client.connect(SocketAddress(host, portNumber).create, TCPTimeOut.toMillis.toInt)
+          val res = true
+          client.close()
+          res
+        } catch {
+          case _: Throwable => false
+        }
+      }
+    }
+
+    Ports.foreach { port =>
+      checkPort(port) foreach { done =>
+        if (done) println(s"$EOL$port is open") else printf(".")
+      }
+    }
+  }
+
+
+  private def getHost: String = {
+    print("Enter your host: ")
+    scala.io.StdIn.readLine()
+  }
+
+  main()
+}
+
+case class SocketAddress(host: String, port: Int) {
+
+  def create = new InetSocketAddress(host, port)
+}
+
+case class JavaSocket() {
+
+  def create = new Socket()
+}
