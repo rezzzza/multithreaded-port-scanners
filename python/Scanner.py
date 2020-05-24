f6406f7e8adc1f0b6c388a0aacad447f334430eb
Reza Hamouleh <me@rezaha.ir>
2020-05-24 04:43:47 +0430

    Add python file structure

--- python/Scanner.py
+++ python/Scanner.py
@@ -1,14 +1,25 @@
 from socket import *
 
-def scan(host: str, port: int): 
-    try:
-        s = socket(AF_INET, SOCK_STREAM)
-        s.settimeout(3)
-        if s.connect_ex((host, port)) == 0:
-            print("port {} is open!".format(port))
-            s.close
-    except Exception:
-        pass
+def scan(host: str): 
+    def checkPort(port):
+        try:
+            s = socket(AF_INET, SOCK_STREAM)
+            s.settimeout(3)
+            if s.connect_ex((host, port)) == 0:
+                s.close
+                return True
+        except Exception:
+            return False
+    
+    for p in range(1, 65535):
+        if checkPort(p):
+            print('port {} is open!'.format(p))
+
+def get_host():
+    return input('Enter your host: ')
 
 
-scan("www.google.com", 80)
+if __name__ == '__main__':
+    host = get_host()
+    print('Trying to scan {}'.format(host))
+    scan(host)
