934f5f64a43e8da89bedd6d3cac9cd6a82c3269b
ali <ali.heidari7091@gmail.com>
2020-05-25 13:31:46 +0430

    Structural changes of socket connection

--- php/Scanner.php
+++ php/Scanner.php
@@ -4,29 +4,26 @@ error_reporting(~E_ALL);
 function scanPort($host)
 {
 
-    $fPort = 1;
-    $lPort = 65535;
-
     function checkPort($host , $port)
     {
         try {
-            $socket = socket_create(AF_INET, SOCK_STREAM, 0);
-            $connect = socket_connect($socket, $host, $port);
-            if ($connect)
-            {
-              socket_close($socket);
+
+            $address = "tcp://".trim($host).":$port";
+            $timeout = 3;
+
+            $connect = stream_socket_client($address, $errno, $errstr, $timeout);
+
+            if ($connect) {
+              fclose($connect);
               return true;
             }
-            return false;
         } catch (Exception $exception) {
             return false;
         }
     }
 
-    for($port=$fPort ; $port <= $lPort ; $port++)
-    {
-        if (checkPort($host, $port))
-        {
+    foreach(range(79,65535) as $port){
+        if (checkPort($host, $port)) {
             echo "port [$port] : open!\n";
         }
     }
